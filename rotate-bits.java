ccec75aef3b659087308b15541dc22ca6faca7ff
Jordan Blakey <jordan@jordanblakey.com>
2017-03-26 02:04:56 -0500

    adding problem solution Rotate Bits in Integer.

--- rotate-bits.java
+++ rotate-bits.java
@@ -4,4 +4,17 @@ Given a number, write a function to rotate the bits (i.e. circular shift).
 e.g.
 
 rotate(0xFFFF0000, 8) = 0x00FFFF00
+
+Questions for the interviewer:
+- Which direction should we rotate the integer?
+- How many bits is the integer?
+
+0xFABCABAB >> N = 0x00FABCAB
+0XFABCABAB << (32 - N) = 0XAB000000
+
 */
+private static BITS_IN_INTEGER = 32;
+
+public int rotate(int x, int N) {
+    return (x << N | x >> (BITS_IN_INTEGER - N))
+}

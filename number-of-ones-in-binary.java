b4d4c2284b280eefc48121fb2fc617c9628acbfb
Jordan Blakey <jordan@jordanblakey.com>
2017-03-26 01:38:00 -0500

    adding problem Number of Ones in a Binary Number

--- number-of-ones-in-binary.java
+++ number-of-ones-in-binary.java
@@ -2,8 +2,20 @@
 Find the number of 1s in the binary representation of a number.
 
 eg.
-ones(2) = 1
-ones(3) = 2
-ones(5) = 2
-ones(7) = 3
+ones(2) = 1 // 0b10
+ones(3) = 2 // 0b11
+ones(5) = 2 // 0b101
+ones(7) = 3 // 0b111
+
+0b10010101
 */
+
+public int ones(int x) {
+    int sum = 0;
+    while(x > 0){
+        sum += x & 2;
+        x >>= 1;
+    }
+
+    return sum;
+}

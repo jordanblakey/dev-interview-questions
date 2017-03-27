757696bb11433eb4dc40cb69b1ba1ee3686903b5
Jordan Blakey <jordan@jordanblakey.com>
2017-03-27 04:36:17 -0500

    Adding Print Reversed Linked List problem.

--- print-reversed-linked-list.java
+++ print-reversed-linked-list.java
@@ -6,4 +6,21 @@ printReversedList(1 -> 2 -> 3)
 3
 2
 1
+
+Reverse linked list method
+1 <- 2 <- 3
+
+Recursive method
+printReversedList(1 -> 2 -> 3)
 */
+
+private class Node {
+  int value;
+  Node next;
+}
+
+public void printReversedList(Node n) {
+  if (n==null) return;
+  printReversedList(n.next);
+  System.out.println(n.value);
+}

f348c83fc017b3428843fb789f93b94efb4ed481
Jordan Blakey <jordan@jordanblakey.com>
2017-03-27 04:52:38 -0500

    Adding Swap variables problem.

--- swap-variables.java
+++ swap-variables.java
@@ -1,3 +1,26 @@
 /*
 Given two integers, write a function that swaps them without using any temporary variables.
+
+Can do this by addition and then sanitzing.
+x = x + y;
+y = x - y;
+x = x - y;
+
+XOR bitwise will cancel out x.
+x ^ x = 0;
+x ^ y ^ x = y;
 */
+
+public void swap(int x, int y) {
+  x = x + y;
+  y = x - y;
+  x = x - y;
+}
+
+// OR
+
+public void swapXOR(int x, int y) {
+  x = x ^ y;
+  y = x ^ y;
+  x = x ^ y;
+}

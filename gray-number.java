e4b4d965df31dd7efbdd96e9594438558d6ba585
Jordan Blakey <jordan@jordanblakey.com>
2017-04-03 02:47:48 -0500

    Quick commit.

--- gray-number.java
+++ gray-number.java
@@ -4,4 +4,28 @@ Given two integers, determine whether or not they differ by a single bit.
 e.g.
 gray(0,1) = true
 gray(1,2) = false
+
+0b01010101
+0b01010001
+
+0b00000100
+
+0b00001001
+
+x & (x - 1)
+
+0b00100000
+0b00011111
+0b00100100
 */
+
+
+    public boolean isGray(int a, int b) {
+        int x = a ^ b;
+        while (x > 0) {
+            if (x % 2 == 1 && x>>1 > 0) return false;
+            x = x >> 1;
+        }
+        return true;
+    }
+}
--- palindromes.java
+++ palindromes.java
@@ -5,3 +5,33 @@ e.g.
 palindrome(1 -> 2 -> 3) = false
 palindrome(1 -> 2 -> 1) = true
 */
+
+1 -> 2 -> 3 -> 2 -> 1 -> null
+          ^
+                        ^
+
+private class Node {
+    private int value;
+    private Node next;
+}
+
+public boolean palindrome(Node n) {
+    Node curr = n;
+    Node runner = n;
+    Stack<Integer> stack = new Stack<Integer>();
+
+    while(renner != null & runner.next != null) {
+        stack.push(curr.value);
+        curr = curr.next;
+        runner = runner.next.next;
+    }
+
+    if (runner != null) curr = curr.next;
+
+    while (curr != null) {
+        if (stack.pop().intValue() != curr.value) return false;
+        curr = curr.next;
+    }
+
+    return true;
+}

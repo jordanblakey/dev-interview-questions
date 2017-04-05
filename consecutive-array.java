6053ec6a3fb09068c256b614e3dabfa269455142
Jordan Blakey <jordan@jordanblakey.com>
2017-04-05 05:06:39 -0500

    Quick commit.

--- consecutive-array.java
+++ consecutive-array.java
@@ -6,4 +6,33 @@ e.g.
 consecutive([4, 2, 1, 6, 5]) = 3 [4, 5, 6]
 consecutive([5, 5, 3, 1])    = 1 [1], [3], or [5]
 
+max = 3
+i = 1
+length = 2
+
+[4, 2, 1, 6, 5]
+[1, 2, 3, 4, 5, 6]
+
+[1, 2, 3, 4, 5, 6]
+       ^
+
+1 -> 2 -> 3 -> ...
+2 -> 3 -> ...
 */
+
+public int consecutive(int[] a) {
+    HashSet<Integer> values = new HashSet();
+    for (int i:a) {
+        value.add(i);
+    }
+
+    int max 0;
+    for (int i: values) {
+        if (values.contains(i-1)) continue;
+        int length = 0;
+        while (values.contains(i++)) length++;
+        max = Math.max(max, length);
+    }
+
+    return max;
+}

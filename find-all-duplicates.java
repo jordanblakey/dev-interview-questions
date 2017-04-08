a17f89340f38a8d7c73230bbc49e55c06e522272
Jordan Blakey <jordan@jordanblakey.com>
2017-04-08 12:31:27 -0500

    Quick commit.

--- find-all-duplicates.java
+++ find-all-duplicates.java
@@ -6,4 +6,24 @@ dups([1, 2, 3] = [])
 dups([1, 2, 2] = [2])
 dups([3, 3, 3] = [3])
 dups([2, 1, 2, 1] = [1, 2])
+
+Brute force: O(N^2)
+
+Set:
+  0(N) - time
+  0(N) - space
+
+Sort:
+  0(N log N)
+  0(1)
+
+Encoding:
+  0(N)
+  0(1)
+  1 <= x <= len(array)
+  0 <= x <= len(array) - 1
+
+[2, -1, 2, 1, 2]
+result = [2, 1, 2]
+index = 1 - 1 = 0
 */

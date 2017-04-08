739e5b1d63f99f3608f001a7476ffc64837f0272
Jordan Blakey <jordan@jordanblakey.com>
2017-04-08 12:24:18 -0500

    Quick commit.

--- stack-from-queues.java
+++ stack-from-queues.java
@@ -1,3 +1,32 @@
 /*
     Implement a stack with basic functionality (push and pop) using queues to store the data.
+
+push(2)
+
+primary = 1 -> 2
+secondary =
 */
+
+public class Stack {
+  private Queue<Integer> primary = new LinkedList<Integer>();
+  private Queue<Integer> secondary = new LinkedList<Integer>();
+
+  public Stack() {}
+
+    public void push(int x) {
+      secondary.add(x);
+      while (!primary.isEmpty()) {
+        second.add(primary.remove());
+      }
+
+      Queue<Integer> temp = primary;
+      primary =  secondary;
+      secondary = temp;
+    }
+
+    public int pop() {
+      if (primary.isEmpty()) throw new IndexOutOfBoundsException();
+      return primary.remove();
+    }
+
+}

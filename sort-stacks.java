fa64e71cfb5852ae88759d1d36ac51e378bbe366
Jordan Blakey <Jordan Blakey>
2017-04-11 18:25:51 -0500

    Quick commit.

--- sort-stacks.java
+++ sort-stacks.java
@@ -1,3 +1,18 @@
 /*
 Given a  stack, sort the elements in the stack using no more than one additional stack.
+Can sort, and reverse, with two stacks and a temp variable.
+
+temp =
+
+   1
+   2
+   3
+   4
+_  _
+
 */
+
+public Stack<Integer> sortStack(Stack<Integer> stack) {
+  if (stack == null || stack.isEmpty()) return stack;
+  Stack<Integer> newStack = new Stack<Integer>();
+}

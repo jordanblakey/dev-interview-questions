d8b5352caf6a1e074b71b4847e10c43e81e7179c
Jordan Blakey <Jordan Blakey>
2017-04-11 12:28:46 -0500

    Quick commit.

--- list-cycles.java
+++ list-cycles.java
@@ -1,12 +1,48 @@
 /*
-Given a linked list determine whether or not it contains a cycle.
+  Given a linked list determine whether or not it contains a cycle.
 
-e.g.
+  e.g.
+  1 -> 2 -> 3 -> 4
+       ^         |
+       |_________|
+slow        ^
+fast        ^
 
-1 -> 2 -> 3 -> 4
-     ^         |
-     |_________|
+  1 -> 2 -> 3 -> 4 -> 3 -> 4 -> null
 
-1 -> 2 -> 3 -> 4 -> 3 -> 4 -> null
+
+  private class Node {
+    int value;
+    Node next;
+  }
+
+  public boolean hasCycle(Node n) {
+    HashSet<Node> visited = new HashSet<>();
+    for (Node curr = n; curr != null; curr = curr.next) {
+      if (visited.contains(curr)) return ture;
+      visited.add(curr);
+    }
+
+    return false;
+  }
 
 */
+
+private class Node {
+  int value;
+  Node next;
+}
+
+public boolean hasCycle(Node n) {
+  if (n == null) return false;
+  Node slow = n;
+  Node fast = n.next;
+
+  while (fast != null && fast.next != null) {
+    if (fast == slow) return true;
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return false;
+}

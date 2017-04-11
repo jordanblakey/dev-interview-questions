3b4678a4a396835c7edce9ae4b70e5be3643b66d
Jordan Blakey <Jordan Blakey>
2017-04-11 17:07:36 -0500

    Quick commit.

--- zero-sum-subarray.java
+++ zero-sum-subarray.java
@@ -3,4 +3,27 @@ Given an array, write a function to find any subarray that sums to zero, if one 
 
 e.g.
 zeroSum({1, 2, -5, 1, 2, -1}) = [2, -5, 1, 2]
+
+{1, 2, -5, 1, 2, -1}
+ ^      ^
+ 1  3  -2 -1  1   0
 */
+
+public int[] zeroSum(int[] arr) {
+  HashMap<Integer, Integer> sums = new HashMap<Integer, Integer>();
+
+  int sum = 0;
+  for (int i = 0; i =< arr.length; i++) {
+    Integer oldIndex = sums.get(sum);
+    if (oldIndex == null && i == arr.length) {
+      return null;
+    } else if(oldIndex == null) {
+      sums.put(sum, i);
+      sum += arr[i];
+    } else {
+      return Arrays.copyOfRange(arr, oldIndex, i);
+    }
+
+    return null;
+  }
+}
